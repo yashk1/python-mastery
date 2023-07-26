8b937a53e66e16d4594ed5d71294b76dad3425ae
Yash Sharma <yash95kumar@gmail.com>
2023-07-25 17:23:36 -0700

    solution 1_3

---
+++ yash-solutions/1_3/pcost.py
@@ -0,0 +1,21 @@
+
+
+
+def portfolio_cost(filename):
+    total_sum = 0.0 
+
+    with open(filename,'r') as file:
+
+        for line in file:
+            fields = line.split()
+            try:
+                nshares = int(fields[1])
+                price = float(fields[2])
+                total_sum += nshares*price
+            except ValueError as e:
+                print("Couldn't parse:", repr(line))
+                print("Reason:", e)
+
+    print(total_sum)
+
+portfolio_cost('../../Data/portfolio2.dat')
